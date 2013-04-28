a9997ba9d6222bfbb96a7e33601c5d6828b8e12e
jj <zhongjj@lemote.com>
2013-04-28 10:42:58 +0800

    Add a simple Register function in subapp login
    this function is to be improved later

--- auth/login.py
+++ auth/login.py
@@ -2,7 +2,7 @@ import os
 import sys
 import web
 from web import form
-from auth import authenticate, get_userid
+from auth import authenticate, get_userid, User
 
 mdir = os.path.dirname(__file__)
 
@@ -10,6 +10,7 @@ urls = (
         "", "Login",
         "/login", "Login",
         "/logout", "Logout",
+        "/register", "Register",
 )
 
 render = web.template.render('%s/templates/'%(mdir))
@@ -45,5 +46,26 @@ class Logout:
     def GET(self):  
         web.ctx.session.kill()
         raise web.seeother("/index",absolute=True)
+
+class Register:
+    def GET(self):
+        session = web.ctx.session
+        if session.get('loggedin',0) == 1:
+            raise web.seeother("/home", absolute=True)
+        return render.register()
+
+    def POST(self):
+        session = web.ctx.session
+        input = web.input()
+        username = input.username
+        password = input.password
+        email = input.email
+        userid = get_userid(username)
+        if userid == -1:
+            newuser = User(password=password, username=username, email=email)
+            newuser.save()
+        else:
+            return "user exists"
+        raise web.seeother("/login")
 
 app = web.application(urls, globals(), autoreload=True)
