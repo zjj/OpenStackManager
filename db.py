974701db6489c19605e93d80a25b07a029e972e8
jj <jj@yuzao.org>
2013-05-16 19:51:54 +0800

    Add MySQL Support for the app (test ok)

--- db.py
+++ db.py
@@ -5,6 +5,8 @@ db_config = ConfigParser.ConfigParser()
 db_config.read('settings.conf')
 dbn = db_config.get('db','dbn')
 database = db_config.get('db','db')
+host = db_config.get('db', 'host')
+port =int(db_config.get('db','port'))
 user = db_config.get('db','user')
 pw = db_config.get('db','passwd')
 user_table = db_config.get('db','user_table')
@@ -15,5 +17,5 @@ server_table = db_config.get('db','server_table')
 if dbn == 'sqlite':
     db = web.database(dbn=dbn, db=database)
 if dbn == 'mysql':
-    db = web.database(dbn=dbn, db=database, user=user, pw=pw)
+    db = web.database(dbn=dbn, host=host, port=port, db=database, user=user, pw=pw)
 
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,29 +1,33 @@
-CREATE TABLE user(
-     id           INTEGER PRIMARY KEY AUTOINCREMENT,
-     username     VARCHAR(100) UNIQUE,
-     password     VARCHAR(128),
-     email        VARCHAR(75) UNIQUE,
-     is_superuser BOOL NOT NULL DEFAULT 0
-);
+BEGIN;
+CREATE TABLE `user` (
+    `id` integer AUTO_INCREMENT PRIMARY KEY,
+    `username` varchar(100) UNIQUE,
+    `password` varchar(200),
+    `email` varchar(100),
+    `is_superuser` bool DEFAULT 0
+)
+;
+CREATE TABLE `sessions` (
+    `session_id` varchar(128),
+    `atime` timestamp DEFAULT CURRENT_TIMESTAMP,
+    `data` longtext
+)
+;
+CREATE TABLE `wiki` (
+    `id` integer AUTO_INCREMENT PRIMARY KEY,
+    `ctx` longtext
+)
+;
+CREATE TABLE `server` (
+    `id` integer AUTO_INCREMENT PRIMARY KEY,
+    `user` integer,
+    `server_name` varchar(128),
+    `image` varchar(128),
+    `flavor` varchar(128)
+)
+;
 
-CREATE TABLE sessions(
-     session_id CHAR(128) UNIQUE NOT NULL,
-     atime      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-     data       TEXT
-);
+-- if MYSQL ADD THIS line: ALTER TABLE `server` ADD CONSTRAINT `user_id_refs_id_fba847f1` FOREIGN KEY (`user`) REFERENCES `user` (`id`);
 
-CREATE TABLE server(
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    user        NOT NULL REFERENCES user(id),
-    server_name VARCHAR(128),
-    image       VARCHAR(128),
-    flavor      VARCHAR(128)
-);
-
-CREATE TABLE wiki(
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    ctx         TEXT
-);
-
-INSERT INTO wiki (ctx) values ("please edit the index page manually. markdown");
+COMMIT;
 
